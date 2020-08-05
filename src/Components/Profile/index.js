8a0dffc9c116f0dd343c27da227f2318c00c7b24
Bishalsarang <bishalsarang@gmail.com>
2020-08-05 11:04:16 +0545

    Display fetched data

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,21 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { connect } from 'react-redux';
 import * as API from '../../Services/API';
 import * as constant from '../../constants/constants';
 
-const Profile = () => {
-  const getUserInfo = () => {
-    API.fetchUser(constant.USER_NAME).then((response) => response.data);
+const Profile = (props) => {
+  const {
+    name,
+    bio,
+    blog,
+    login,
+    email,
+    company,
+    location,
+    hireable,
+    updated_at,
+    followers,
+    following,
+    avatar_url,
+    created_at,
+    public_repos,
+    public_gists,
+    twitter_username
+  } = props.profile;
+
+  useEffect(() => {
+    console.log('Fetching');
+    getUserInfo().then((response) => props.setProfile(response));
+  }, []);
+
+  const getUserInfo = async () => {
+    const response = await API.fetchUser(constant.USER_NAME).then((response) => {
+      return response.data;
+    });
+    return response;
   };
 
   return (
     <div className="Profile">
-      <span className="username"></span>
-      <span className="username"></span>
-      <span className="username"></span>
-      <span className="username"></span>
+      <span className="name">Name: {name}</span>
+      <span className="username">Username: {login}</span>
+
+      {company && <span className="company">{company}</span>}
+      {bio && <span className="bio">{bio}</span>}
+      {blog && <span className="blog">{blog}</span>}
+      {email && <span className="email">{email}</span>}
+      {location && <span className="location">{location}</span>}
+      {hireable && <span className="hireable">{hireable}</span>}
+      {updated_at && <span className="updated_at">{updated_at}</span>}
+      {followers && <span className="followers">{followers}</span>}
+      {following && <span className="following">{following}</span>}
+
+      {public_repos && <span className="public_repos">{public_repos}</span>}
+      {public_gists && <span className="public_gists">{public_gists}</span>}
+      <img src={avatar_url} alt={`${login} Photo`} className="picture"></img>
     </div>
   );
 };
 
-export default Profile;
+const mapStateToProps = (state) => {
+  return {
+    profile: state ? state.profile : {}
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setProfile: (value) =>
+      dispatch({
+        type: 'SET_PROFILE',
+        payload: { profile: value }
+      })
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
--- src/store.js
+++ src/store.js
@@ -1,6 +1,20 @@
 import { createStore } from 'redux';
 
-const store = createStore(() => {},
-window.__REDUX_DEVTOOL_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const INITIAL_STATE = {
+  profile: {}
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_PROFILE':
+      return { ...state, profile: action.payload.profile };
+  }
+};
+
+const store = createStore(
+  reducer,
+  INITIAL_STATE,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
