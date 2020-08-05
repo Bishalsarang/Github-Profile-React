4b70cb52508856c03555beb316d0a9f9ad1e5024
Bishalsarang <bishalsarang@gmail.com>
2020-08-06 00:35:09 +0545

    Updates

---
+++ precache-manifest.d75aefb7099a80fc693a6767a2252a81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c765ad4eed75b4295939101f07919e",
+    "url": "/Github-Profile-React/index.html"
+  },
+  {
+    "revision": "72875d29a151663511bc",
+    "url": "/Github-Profile-React/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "9e61f74910e3ea39d38a",
+    "url": "/Github-Profile-React/static/css/main.76a83c19.chunk.css"
+  },
+  {
+    "revision": "72875d29a151663511bc",
+    "url": "/Github-Profile-React/static/js/2.ab6ff732.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Github-Profile-React/static/js/2.ab6ff732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e61f74910e3ea39d38a",
+    "url": "/Github-Profile-React/static/js/main.9c8c1126.chunk.js"
+  },
+  {
+    "revision": "f0b1a983b568671a2bb4",
+    "url": "/Github-Profile-React/static/js/runtime-main.141d8dd5.js"
+  },
+  {
+    "revision": "236d70a3fdbae4ad1df5397985d70acb",
+    "url": "/Github-Profile-React/static/media/loader.236d70a3.svg"
+  }
+]);
