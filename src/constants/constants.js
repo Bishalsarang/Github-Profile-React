de73979c537ffb940b9233f6db7025f8327b9094
Bishalsarang <bishalsarang@gmail.com>
2020-08-07 22:45:03 +0545

    Fix constants laddering

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,10 +1,13 @@
+// API Constants
 export const USER_NAME = 'Bishalsarang';
 export const API_USER_BASE_URL = 'https://api.github.com/users/';
 export const API_USER_URL = API_USER_BASE_URL + USER_NAME;
+
 export const API_REPO_URL = `${API_USER_URL}/repos`;
 export const API_FOLLOWER_URL = `${API_USER_URL}/followers`;
 export const API_FOLLOWING_URL = `${API_USER_URL}/following`;
 
+// UI Constants
 export const LANGUAGES = ['All', 'C++', 'HTML', 'Python', 'JavaScript', 'Jupyter Notebook'];
 export const MAP_LANGUAGES_TO_COLOR = {
   'C++': '#F34B7D',
@@ -14,5 +17,5 @@ export const MAP_LANGUAGES_TO_COLOR = {
   'Jupyter Notebook': '#DA5B0B',
 };
 
-export const SORT_REPO_BY = ['updated', 'created', 'starCounts', 'forkedCounts'];
 export const ITEMS_PER_PAGE = 10;
+export const SORT_REPO_BY = ['updated', 'created', 'starCounts', 'forkedCounts'];
